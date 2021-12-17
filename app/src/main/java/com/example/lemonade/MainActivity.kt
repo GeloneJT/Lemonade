f3f98435208942caae97e8d83dd68400627b2df9
Jelani Thomas <jelani13@icloud.com>
2021-12-17 09:53:58 -0500

    View elements changed to reflect state

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -117,14 +117,19 @@ class MainActivity : AppCompatActivity() {
      */
     private fun setViewElements() {
         val textAction: TextView = findViewById(R.id.text_action)
-        // TODO: set up a conditional that tracks the lemonadeState
 
-        // TODO: for each state, the textAction TextView should be set to the corresponding string from
-        //  the string resources file. The strings are named to match the state
-
-        // TODO: Additionally, for each state, the lemonImage should be set to the corresponding
-        //  drawable from the drawable resources. The drawables have the same names as the strings
-        //  but remember that they are drawables, not strings.
+        when (lemonadeState){
+            SELECT -> textAction.setText(R.string.lemon_select)
+            SQUEEZE -> textAction.setText(R.string.lemon_squeeze)
+            DRINK -> textAction.setText(R.string.lemon_drink)
+            RESTART -> textAction.setText(R.string.lemon_empty_glass)
+        }
+        when (lemonadeState){
+            SELECT -> lemonImage?.setImageResource(R.drawable.lemon_tree)
+            SQUEEZE -> lemonImage?.setImageResource(R.drawable.lemon_squeeze)
+            DRINK -> lemonImage?.setImageResource(R.drawable.lemon_drink)
+            RESTART -> lemonImage?.setImageResource(R.drawable.lemon_restart)
+        }
     }
 
     /**
